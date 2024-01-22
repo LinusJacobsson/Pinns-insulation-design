89bf53636ac19fe834722b05db9cd92d7d44f926
LinusJacobsson <jacobssonlinus00@gmail.com>
2024-01-22 21:13:35 +0100

    works for N=100

--- test.py
+++ test.py
@@ -27,13 +27,13 @@ def analytic(t, x_0, v_0, omega):
     B = v_0 / omega  # Amplitude from initial velocity
     x = A * np.cos(omega * t) + B * np.sin(omega * t)
     return x
-
-def generate_dataset(N=100, noise_percent=0.0, omega=0.1, seed=420, x_0 = -2, v_0 = 0):
+# N = 200 funkar ej, N = 100 gör det bra
+def generate_dataset(N=1000, noise_percent=0.0, omega=4, seed=420, x_0 = -2, v_0 = 0):
     # seed key for debugging
     np.random.seed(seed)
 
     # Domains of t and x
-    tmin, tmax = 0.0, 1.0
+    tmin, tmax = 0.0, 10.0
 
     t_vals = np.random.uniform(low=tmin,high=tmax,size=(N,1))
 
@@ -50,11 +50,11 @@ data, tmin, tmax = generate_dataset()
 
 class MLP(nn.Module):
     features: Sequence[int]
-    # We also add an initializer for the D parameter
+    # We also add an initializer for the omega parameter
     omega_init: Callable = jax.nn.initializers.ones
 
     def setup(self):
-        # include the D parameter during setup
+        # include the omega parameter during setup
         omega = self.param("omega", self.omega_init, (1,))
         self.layers = [nn.Dense(features=feat, use_bias=True) for feat in self.features]
         
@@ -92,7 +92,7 @@ def loss_fun(params,data):
     mse_u = MSE(u_c, ufunc(t_c))
     mse_f = jnp.mean(PINN_f(t_c,omega,ufunc)**2)
     
-    return mse_f + mse_u
+    return mse_f + 10*mse_u
 
 @jax.jit
 def update(opt_state,params,data):
@@ -116,13 +116,13 @@ def init_process(feats):
     dummy_in = jax.random.normal(key1, (1,))
     params = model.init(key2, dummy_in)
 
-    lr = optax.piecewise_constant_schedule(1e-2,{10_000:5e-3,30_000:1e-3,50_000:5e-4,70_000:1e-4})
+    lr = optax.piecewise_constant_schedule(1e-2,{15_000:5e-3,80_000:1e-3})
     optimizer = optax.adam(lr)
     opt_state = optimizer.init(params)
     
     return model, params, optimizer, opt_state
 
-features = [8, 8, 8, 8, 8, 8, 1]
+features = [8, 8, 8, 8, 8, 1]
 
 model, params, optimizer, opt_state = init_process(features)
 
@@ -130,14 +130,12 @@ model, params, optimizer, opt_state = init_process(features)
 epochs = 100_000
 for epoch in range(epochs):
     opt_state, params = update(opt_state,params,data)
-
+    current_omega = params["params"]["omega"][0]
     # print loss and epoch info
     if epoch%(1000) ==0:
-        print(f'Epoch = {epoch},\tloss = {loss_fun(params,data):.3e}')
+        print(f'Epoch = {epoch},\tloss = {loss_fun(params,data):.3e}, \t omega = {current_omega:.3f}')
 
 Dcalc = params["params"]["omega"][0]
-omega = 0.1
+omega = 4
 print(f"The real value of the parameter is omega = {omega}")
-print(f"The calculated value for the parameter is D_calc = {Dcalc:.7f}.")
-print(f"This corresponds to a {100*(Dcalc-omega)/omega:.5f}% error.")
-
+print(f"The c
